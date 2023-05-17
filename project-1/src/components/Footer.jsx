d2a5ba6c685b24f2dde3c48fbef990fad7eea71c
aryan <aryansaini0007@gmail.com>
2023-05-17 18:35:10 +0530

    Project 1 initial commit

---
+++ project-1/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Aryan Saini</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Me</h5>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/aryansaini0007/"
+            target={"blank"}
+          >
+            LinkedIn
+          </a>
+          <a href="https://github.com/007aryansaini" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
